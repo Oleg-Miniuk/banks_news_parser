20d15f975a523292789a2b29f0cb54286c7abae2
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-10-29 19:10:51 +0300

    test:: connection to db on async/await

--- tests/db/db.js
+++ tests/db/db.js
@@ -1,38 +1,20 @@
 const MongoClient = require('mongodb').MongoClient;
 const appConfig = require('../../config/appConfig');
 
-// const {
-//   db: { url, dbName }
-// } = appConfig;
+const {
+  db: { url, dbName }
+} = appConfig;
 
-// (async () => {
-//   const mongoClient = new MongoClient(url);
-//   let db;
-//   mongoClient.connect((err, client) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     db = client.db(dbName);
-//     db.listCollections((...props) => console.log(props));
-//   });
-// })();
-
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'b-n_1';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  console.log('Connected successfully to server');
-
-  const db = client.db(dbName);
-  const newsCollection = db.collection('news');
-  newsCollection.find({}).toArray((error, news) => {
+(async () => {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const newsCollection = db.collection('news');
+    const news = await newsCollection.find({}).toArray();
     console.log(news);
     client.close();
-  });
-});
+  } catch (error) {
+    console.log(`error: ${error}`);
+  }
+})();
