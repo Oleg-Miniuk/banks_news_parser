5357cff3fbdb776271bec4d32cc006fd74b7ac58
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-10-31 13:53:20 +0300

    test req to channel

--- tests/telegram/telegram.js
+++ tests/telegram/telegram.js
@@ -1,21 +1,32 @@
-const TeleBot = require('telebot');
+// const TeleBot = require('telebot');
+const axios = require('axios');
 const { token } = require('../../config/telegramConfig');
 
-const bot = new TeleBot({
-  token // Required. Telegram Bot API token.
-  // polling: {
-  //   // Optional. Use polling.
-  //   interval: 1000, // Optional. How often check updates (in ms).
-  //   timeout: 0, // Optional. Update polling timeout (0 - short polling).
-  //   limit: 100, // Optional. Limits the number of updates to be retrieved.
-  //   retryTimeout: 5000 // Optional. Reconnecting timeout (in ms).
-  // },
-  // allowedUpdates: []
-});
+const chatId = '@dh_test_channel_1';
 
-bot.on('text', (msg) => {
-  console.log(msg.text);
-  msg.reply.text(msg.text);
-});
+// const bot = new TeleBot({
+//   token, // Required. Telegram Bot API token.
+//   polling: {
+//     // Optional. Use polling.
+//     interval: 1000, // Optional. How often check updates (in ms).
+//     timeout: 0, // Optional. Update polling timeout (0 - short polling).
+//     limit: 100, // Optional. Limits the number of updates to be retrieved.
+//     retryTimeout: 5000 // Optional. Reconnecting timeout (in ms).
+//   },
+//   allowedUpdates: []
+// });
 
-bot.start();
+// bot.on('text', (msg) => {
+//   console.log(msg.text);
+//   msg.reply.text(msg.text);
+// });
+
+// bot.start();
+
+axios
+  .get(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=dick`)
+  .then((response) => {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(err => console.log(err));
