b8b2f0d9d9f5cac43fa5bb0681a88e404e58cfea
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-11-01 17:11:00 +0300

    newsParsere )

--- scripts/parser/parser.js
+++ scripts/parser/parser.js
@@ -57,7 +57,8 @@ rule.minute = [new schedule.Range(0, 59, 2)];
       db
     });
 
-    const results = await Promise.all(newsParsers.map(parser => parser())).catch(err => console.log(`GOTCHA ${err})`);
+    const results = await Promise.all(newsParsers.map(parser => parser())).catch(err => console.log(`GOTCHA ${err})`)
+    );
     const notificationList = results.reduce((accum, prevArr) => [...accum, ...prevArr], []);
 
     client.close();
