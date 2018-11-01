34389c0ca08f200bc7e3ac390097fb9c1f06b77a
Oleg Miniuk <oleg.miniuk@gmail.com>
2018-11-01 21:55:20 +0300

    testing tajiks

--- scripts/parser/news/newsParsers.js
+++ scripts/parser/news/newsParsers.js
@@ -6,6 +6,12 @@ const bankRossiya = require('./bankRossiya');
 const sauberBank = require('./sauberBank');
 const gorBank = require('./gorBank');
 
-const parsers = [bank821, gorBank, spbBank, eskhata, imon, bankRossiya, sauberBank];
+// const parsers = [bank821, gorBank, spbBank, eskhata, imon, bankRossiya, sauberBank];
+
+const parsers = [eskhata, imon];
+
+// const parsers = [bank821, gorBank];
+// const parsers = [spbBank];
+// const parsers = [bankRossiya, sauberBank];
 
 module.exports = parsers;
